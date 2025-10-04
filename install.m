c448371f115d1a696599fa3de576c1ab6200c0f1
Manoj <v.manoj1996@gmail.com>
2025-10-04 19:34:32 -0400

    mujoco updated to 3.3.6

--- install.m
+++ install.m
@@ -1,10 +1,10 @@
 function install()
-% Copyright 2022-2023 The MathWorks, Inc.
+% Copyright 2022-2025 The MathWorks, Inc.
 
 %% Run this script once and follow the instructions
 % Make sure you run this script from this folder.
 
-MJ_VER = '2.3.5';
+MJ_VER = '3.3.6';
 GLFW_VER = '3.3.7';
 urlsList = fileread("tools/links.json");
 blockPath = './blocks/';
@@ -85,37 +85,76 @@ end
 
 %% Local functions
     function downloadfolder = downloader(urlsList, name, version)
-        libraryFolder = 'lib';
-        downloadfolder = fullfile(pwd, libraryFolder, computer('arch'), name);
+    libraryFolder = 'lib';
+    downloadfolder = fullfile(pwd, libraryFolder, computer('arch'), name);
+    
+    if strcmp(name, 'mujoco')
+        % Generate MuJoCo URL based on version and architecture
+        downloadLink = generateMujocoUrl(version);
+    else
+        % Use JSON for other libraries (GLFW)
         urlJson = jsondecode(urlsList);
+        downloadLink = '';
         for i=1:length(urlJson.files)
             obj = urlJson.files(i);
             if strcmp(obj.name, name) && strcmp(obj.version, version) && strcmp(obj.arch, computer('arch'))
                 downloadLink = obj.downloadLink;
+                break;
             end
         end
-        disp(' ')
-        disp('Download URL is:');
-        disp(downloadLink);
-
-        if isfolder(downloadfolder)
-            disp(' ')
-            disp('folder exists already. it will be overwritten');
+        
+        if isempty(downloadLink)
+            error('Download link not found for %s version %s on %s', name, version, computer('arch'));
         end
-
-        [~,~,ext]=fileparts(downloadLink);
-        downloadFile = fullfile(libraryFolder, strcat('download', ext));
-        status = mkdir(libraryFolder); %#ok<NASGU> 
-        websave(downloadFile, downloadLink);
-        if strcmp(ext,'.zip')
-            unzip(downloadFile, downloadfolder);
-        elseif strcmp(ext, '.gz')
-            untar(downloadFile, downloadfolder);
-        else
-            error('unknown extension. Unable to extract archive');
-        end
-        disp(' ')
-        disp(name + ' downloaded and extracted to this path:');
-        disp(downloadfolder)
     end
+    
+    disp(' ')
+    disp('Download URL is:');
+    disp(downloadLink);
+
+    if isfolder(downloadfolder)
+        disp(' ')
+        disp('folder exists already. it will be overwritten');
+    end
+
+    [~,~,ext]=fileparts(downloadLink);
+    downloadFile = fullfile(libraryFolder, strcat('download', ext));
+    status = mkdir(libraryFolder); %#ok<NASGU> 
+    websave(downloadFile, downloadLink);
+    if strcmp(ext,'.zip')
+        unzip(downloadFile, downloadfolder);
+    elseif strcmp(ext, '.gz')
+        untar(downloadFile, downloadfolder);
+    else
+        error('unknown extension. Unable to extract archive');
+    end
+    disp(' ')
+    disp(name + ' downloaded and extracted to this path:');
+    disp(downloadfolder)
+end
+
+function url = generateMujocoUrl(version)
+    baseUrl = 'https://github.com/deepmind/mujoco/releases/download/';
+    
+    switch computer('arch')
+        case 'win64'
+            platform = 'windows-x86_64';
+            ext = '.zip';
+        case 'glnxa64'
+            platform = 'linux-x86_64';
+            ext = '.tar.gz';
+        case 'maci64'
+            platform = 'macos-universal2';
+            ext = '.dmg';
+        case 'maca64'
+            % For Apple Silicon Macs
+            platform = 'macos-universal2';
+            ext = '.dmg';
+        otherwise
+            error('Unsupported architecture: %s', computer('arch'));
+    end
+    
+    % Construct the URL: version/mujoco-version-platform.ext
+    url = sprintf('%s%s/mujoco-%s-%s%s', baseUrl, version, version, platform, ext);
+end
 end
